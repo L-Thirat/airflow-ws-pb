e69e305d3bc2d2750ac1adc0800f80340e1c7664
L-thirat <terus_limsurut@hotmail.com>
2022-06-22 00:32:55 +0900

    remove docker file

--- dags/sample_timetable_dag.py
+++ dags/sample_timetable_dag.py
@@ -11,7 +11,7 @@ def sleepy_op():
 with airflow.DAG(
         dag_id='timetable_test',
         start_date=pendulum.datetime(2022, 6, 2, tz=pendulum.timezone('Asia/Tokyo')),
-        timetable=MultiCronTimetable(['0 0 2 * *', '0 0 * * MON#1,MON#2'], timezone='Asia/Tokyo'),
+        timetable=MultiCronTimetable(cron_defs=['0 0 2 * *', '0 0 * * MON#1,MON#2'], timezone='Asia/Tokyo'),
         catchup=False,
         max_active_runs=1) as dag:
 
