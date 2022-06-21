2e1f39959ee2c1eeba3433754e771fb67a2fd802
L-thirat <terus_limsurut@hotmail.com>
2022-06-22 00:16:33 +0900

    first commit

---
+++ dags/sample_timetable_dag.py
@@ -0,0 +1,21 @@
+from time import sleep
+import airflow
+from airflow.operators.python import PythonOperator
+import pendulum
+from timetable import MultiCronTimetable
+
+def sleepy_op():
+    sleep(660)
+
+
+with airflow.DAG(
+        dag_id='timetable_test',
+        start_date=pendulum.datetime(2022, 6, 2, tz=pendulum.timezone('Asia/Tokyo')),
+        timetable=MultiCronTimetable(['0 0 2 * *', '0 0 * * MON#1,MON#2'], timezone='Asia/Tokyo'),
+        catchup=False,
+        max_active_runs=1) as dag:
+
+    sleepy = PythonOperator(
+        task_id='sleepy',
+        python_callable=sleepy_op
+    )
